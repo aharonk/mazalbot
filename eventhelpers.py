357b1901c93a6bb7984a379b0bee475b6fa31b8d
Aharon Katz <akatz@safermobility.com>
2026-03-29 12:40:40 -0400

    done

--- eventhelpers.py
+++ eventhelpers.py
@@ -5,10 +5,10 @@ from sheets import get_data
 def get_sheet_as_events():
     return Event.from_list(get_data())
 
-def act_on_events_of_day(events, date, action):
+async def act_on_events_of_day(events, date, action):
     for e in events:
         if e.date.month == date.month and e.date.day == date.day:
-            action(e)
+            await action(e)
 
-def act_on_sheet_for_day(date, action):
-    return act_on_events_of_day(get_sheet_as_events(), date, action)
+async def act_on_sheet_for_day(date, action):
+    return await act_on_events_of_day(get_sheet_as_events(), date, action)
--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import discord
 import os
 
-from datetime import datetime
+from datetime import datetime, time
 from discord.ext import tasks
 from dotenv import load_dotenv
 from eventhelpers import act_on_sheet_for_day
+from zoneinfo import ZoneInfo
 
 load_dotenv()
 bot = discord.Bot()
 
+t = time(13, tzinfo=ZoneInfo('America/New_York'))
+
 @bot.event
 async def on_ready():
     print(f"{bot.user} connected.")
+    send_message.start()
 
 
-@tasks.loop(minutes=15)
+@tasks.loop(time=t)
 async def send_message():
-    print("Running at" + str(datetime.now().time()))
+    print("Running at " + str(datetime.now().time()))
     u = await bot.fetch_user(677683613277487121)
     channel = await bot.create_dm(u)
 
     today = datetime.strptime('3/30/2027', "%m/%d/%Y")
     # today = datetime.now()
 
-    act_on_sheet_for_day(today, lambda e: channel.send(e.to_message(today)))
+    await act_on_sheet_for_day(today, lambda e: channel.send(e.to_message(today)))
 
 bot.run(str(os.getenv("TOKEN")))
